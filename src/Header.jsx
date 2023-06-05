4fdb20233bf0316a0e1617713eff4478096d4350
Bin <zzbsdh000@outlook.com>
2023-06-05 17:39:53 -0230

    Add layout

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header(props) {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <h1>Memory Nothing Card</h1>
+        <p>
+          Get points by clicking on an image but don&apos;t click on any more
+          than once!
+        </p>
+      </div>
+      <div className="header-right">
+        <p>Score: 2</p>
+        <p>Best score: 99</p>
+      </div>
+    </header>
+  );
+}
