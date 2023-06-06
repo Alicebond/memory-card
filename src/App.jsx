8398afc12f2e3209d5effc49401a304ef2f842ce
Bin <zzbsdh000@outlook.com>
2023-06-06 09:57:31 -0230

    Fix unique key props bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-// import { useState } from 'react'
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+import { useState } from "react";
 import "./App.css";
 import Card from "./Card";
 import Header from "./Header";
 
 function App() {
+  const [score, setScore] = useState(1);
   const cards = [];
   for (let i = 0; i < 16; i++) {
-    cards.push(<Card number={i + 1} />);
+    cards.push(<Card number={i + 1} key={uuidv4()} id={uuidv4()} />);
   }
   return (
     <>
-      <Header />
+      <Header score={score} />
       <div className="cards">{cards}</div>
     </>
   );
